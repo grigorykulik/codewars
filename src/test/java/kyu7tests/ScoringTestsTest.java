efb65d90dba48066af9c3487fef990f7d0c24efe
grigorykulik <stacheldraht23@egmail.com>
2022-02-28 23:26:58 +0300

    initial commit

---
+++ src/test/java/kyu7tests/ScoringTestsTest.java
@@ -0,0 +1,29 @@
+package kyu7tests;
+
+import kyu7.ScoringTests;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoringTestsTest {
+
+    @Test
+    public void test1() {
+        int[] arr = {0, 0, 0, 0, 2, 1, 0};
+        int r = 2;
+        int o = 0;
+        int w = 1;
+
+        assertEquals(9, ScoringTests.getScore(arr, r, o, w));
+    }
+
+    @Test
+    public void test2() {
+        int[] arr = {0, 1, 0, 0, 2, 1, 0, 2, 2, 1};
+        int r = 3;
+        int o = -1;
+        int w = 2;
+
+        assertEquals(3, ScoringTests.getScore(arr, r, o, w));
+    }
+}
---
+++ src/test/java/kyu7tests/ShorterConcatTest.java
@@ -0,0 +1,16 @@
+package kyu7tests;
+
+import kyu7.ShorterConcat;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShorterConcatTest {
+
+    @Test
+    public void test1() {
+        assertEquals("abcdetsrifabcde",ShorterConcat.shorterReverseLonger("first", "abcde"));
+        assertEquals("bauollehbau",ShorterConcat.shorterReverseLonger("hello", "bau"));
+        assertEquals("fghiedcbafghi",ShorterConcat.shorterReverseLonger("abcde", "fghi"));
+    }
+}
