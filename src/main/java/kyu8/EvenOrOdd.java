c0ce46cffb159c06b6f1b4d1b2a013cc0d91ac71
grigorykulik <stacheldraht23@gmail.com>
2022-03-19 22:09:35 +0300

    no message

---
+++ src/main/java/kyu8/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+/**
+ * https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+ * Create a function that takes an integer as an argument
+ * and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) return "Even";
+        else return "Odd";
+    }
+}
