3abeb253a6083bd9c992ccee16adc7da18d2ef03
grigorykulik <stacheldraht23@gmail.com>
2022-04-27 00:11:58 +0300

    no message

--- src/main/java/kyu7/DescendingOrder.java
+++ src/main/java/kyu7/DescendingOrder.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
  */
 public class DescendingOrder {
     public static int sortDesc(final int num) {
-        return Integer.parseInt( String.valueOf(num)
+        return Integer.parseInt(String.valueOf(num)
                 .chars()
                 .mapToObj(c -> (char) c)
                 .sorted((c1, c2) -> Character.compare(c2, c1))
